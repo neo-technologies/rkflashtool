7c29981b0231dfa9da7385de54e7ddf83ecc387b
Ivo van Poorten <ivop@free.fr>
2013-12-12 19:16:33 +0100

    clean up, expand tabs to 4 spaces, some speed and size improvements

--- rkcrc.c
+++ rkcrc.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2010, 2011 FUKAUMI Naoki.
+ * Copyright (c) 2010, 2011 Fukaumi Naoki
+ * Copyright (c) 2013 Ivo van Poorten
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,93 +30,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "rkcrc.h"
 
-static char *progname;
+static const char strings[2][4] = { "KRNL", "PARM" };
 
-static void
-usage(void)
-{
+int main(int argc, char *argv[]) {
+    struct stat st;
+    ssize_t nr;
+    uint32_t crc = 0;
+    uint8_t buf[512];
+    char *progname = argv[0];
+    int ch, which = -1, in, out;
 
-	fprintf(stderr, "usage: %s [-k|-p] infile outfile\n", progname);
-	exit(EXIT_FAILURE);
-}
+    while ((ch = getopt(argc, argv, "kp")) != -1) {
+        switch (ch) {
+        case 'k': which = 0; break;
+        case 'p': which = 1; break;
+        default:
+usage:
+            fprintf(stderr, "usage: %s [-k|-p] infile outfile\n", progname);
+            exit(1);
+        }
+    }
+    argc -= optind;
+    argv += optind;
 
-int
-main(int argc, char *argv[])
-{
-	struct stat st;
-	ssize_t nr;
-	uint32_t crc;
-	uint8_t buf[512];
-	int ch, krnl, in, out, parm;
+    if (argc != 2 || which < 0) goto usage;
 
-	progname = argv[0];
+    if ((in = open(argv[0], O_RDONLY)) == -1)
+        err(1, "%s", argv[0]);
 
-	krnl = parm = 0;
-	while ((ch = getopt(argc, argv, "kp")) != -1) {
-		switch (ch) {
-		case 'k':
-			krnl = 1;
-			break;
-		case 'p':
-			parm = 1;
-			break;
-		default:
-			usage();
-		}
-	}
-	argc -= optind;
-	argv += optind;
+    if (fstat(in, &st) != 0)
+        err(1, "%s", argv[0]);
 
-	if (argc != 2 || (krnl && parm))
-		usage();
+    if ((out = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+        err(1, "%s", argv[1]);
 
-	if ((in = open(argv[0], O_RDONLY)) == -1)
-		err(EXIT_FAILURE, "%s", argv[0]);
+    memcpy(buf, strings[which], 4);
+    PUT32LE(buf+4, st.st_size);
+    write(out, buf, 8);
 
-	if (fstat(in, &st) != 0)
-		err(EXIT_FAILURE, "%s", argv[0]);
+    while ((nr = read(in, buf, sizeof(buf))) > 0) {
+        RKCRC(crc, buf, nr);
+        write(out, buf, nr);
+    }
 
-	if ((out = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
-		err(EXIT_FAILURE, "%s", argv[1]);
+    PUT32LE(buf, crc);
+    write(out, buf, 4);
 
-	if (krnl || parm) {
-		if (krnl) {
-			buf[0] = 'K';
-			buf[1] = 'R';
-			buf[2] = 'N';
-			buf[3] = 'L';
-		} else {
-			buf[0] = 'P';
-			buf[1] = 'A';
-			buf[2] = 'R';
-			buf[3] = 'M';
-		}
-		buf[4] = (st.st_size >> 0) & 0xff;
-		buf[5] = (st.st_size >> 8) & 0xff;
-		buf[6] = (st.st_size >> 16) & 0xff;
-		buf[7] = (st.st_size >> 24) & 0xff;
+    close(out);
+    close(in);
 
-		write(out, buf, 8);
-	}
-
-	crc = 0;
-	while ((nr = read(in, buf, sizeof(buf))) != -1 && nr != 0) {
-		RKCRC(crc, buf, nr);
-		write(out, buf, nr);
-	}
-
-	buf[0] = (crc >> 0) & 0xff;
-	buf[1] = (crc >> 8) & 0xff;
-	buf[2] = (crc >> 16) & 0xff;
-	buf[3] = (crc >> 24) & 0xff;
-
-	write(out, buf, 4);
-
-	close(out);
-	close(in);
-
-	return EXIT_SUCCESS;
+    return 0;
 }
